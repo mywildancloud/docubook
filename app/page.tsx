a9a9e90a63708627f496cdddcf041e5b6986d1d9
Wildan Nursahidan <80866662+mywildancloud@users.noreply.github.com>
2024-11-24 01:20:41 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="z-10 flex min-h-5 items-center justify-center">
           <div
             className={cn(
-              "group rounded-full border border-black bg-black/5 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-accent dark:border-white/5 dark:bg-transparent dark:hover:bg-accent",
+              "group rounded-full border border-black/75 bg-black/5 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-accent dark:border-white/5 dark:bg-transparent dark:hover:bg-accent",
             )}
           >
             <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-100 hover:duration-300 hover:dark:text-neutral-200">
