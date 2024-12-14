da1f3540ce9e898ff9fca8f15ee54ad727abca62
Wildan Nursahidan <80866662+mywildancloud@users.noreply.github.com>
2024-12-14 22:58:58 +0700

    Update button.tsx

--- components/markdown/button.tsx
+++ components/markdown/button.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import * as Icons from "lucide-react";
 import Link from "next/link";
 
+// Mendeklarasikan tipe IconName berdasarkan nama ikon yang tersedia dalam objek Icons
 type IconName = keyof typeof Icons;
-type ButtonProps = {
-  icon?: keyof typeof Icons;
+
+// Mendeklarasikan interface untuk props Button
+interface ButtonProps {
+  icon?: IconName; // Menggunakan IconName untuk memastikan hanya ikon yang valid
   text?: string;
   href: string;
   target?: "_blank" | "_self" | "_parent" | "_top";
   size?: "sm" | "md" | "lg";
   variation?: "primary" | "accent" | "outline";
-};
+}
 
 const Button: React.FC<ButtonProps> = ({
   icon,
@@ -20,21 +23,25 @@ const Button: React.FC<ButtonProps> = ({
   size = "md",
   variation = "primary",
 }) => {
-  const baseStyles = "inline-flex items-center justify-center rounded font-medium focus:outline-none transition no-underline";
+  const baseStyles =
+    "inline-flex items-center justify-center rounded font-medium focus:outline-none transition no-underline";
 
-  const sizeStyles = {
+  // Menyusun style berdasarkan ukuran
+  const sizeStyles: Record<ButtonProps["size"], string> = {
     sm: "px-3 py-1 my-6 text-sm",
     md: "px-4 py-2 my-6 text-base",
     lg: "px-5 py-3 my-6 text-lg",
   };
 
-  const variationStyles = {
+  // Menyusun style berdasarkan variasi
+  const variationStyles: Record<ButtonProps["variation"], string> = {
     primary: "bg-primary text-white hover:bg-primary/90",
     accent: "bg-accent text-white hover:bg-accent/90",
     outline: "border border-accent text-accent hover:bg-accent/10",
   };
 
-  const Icon = icon ? (Icons[icon] as React.FC<{ className?: string }>) : null; // Tipe eksplisit sebagai React.FC
+  // Menentukan komponen ikon jika ada
+  const Icon = icon ? (Icons[icon] as React.FC<{ className?: string }>) : null;
 
   return (
     <Link
