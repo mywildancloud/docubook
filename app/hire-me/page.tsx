3bc94cd7cdbeefab08796ec559e76626c2c660bb
Wildan Nursahidan <edpwildan@gmail.com>
2024-12-18 23:21:34 +0700

    improve

--- app/hire-me/page.tsx
+++ app/hire-me/page.tsx
@@ -2,17 +2,16 @@ import { getMetadata } from "@/app/layout";
 
 export const metadata = getMetadata({
   title: "Hire Me",
-  description: "Hire me for your project or business. Contact me for a free quote.",
+  description: "Hire me to start a documentation project with DocuBook",
 });
 
 export default function EmbeddedHTML() {
     return (
-      <div className="flex justify-center items-center min-h-svh">
+      <div className="w-full py-10 mx-auto min-h-svh">
         <iframe
           src="/hire-me.html"
           width="100%"
-          height="1000px"
-          style={{ border: "none" }}
+          height="1000"
         />
       </div>
     );
