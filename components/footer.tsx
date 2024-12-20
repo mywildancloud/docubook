2767b2fe97ad72398a9a9669e764b2e1f8379e91
Wildan Nursahidan <edpwildan@gmail.com>
2024-12-21 01:00:15 +0700

    improve docu.json

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,24 +8,22 @@ export function Footer() {
   return (
     <footer className="border-t w-full h-16">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+        {/* Footer Text */}
         <div className="flex items-center gap-3">
           <p className="text-center">
-            {footer.text.replace("{year}", new Date().getFullYear().toString())}{" "}
-            {footer.links.map((link, index) => (
-              <Link
-                key={index}
-                className={`px-1 ${
-                  link.underline ? "underline underline-offset-2" : ""
-                }`}
-                href={link.url}
-                target="_blank"
-              >
-                {link.text}
-              </Link>
-            ))}
+            Copyright © {new Date().getFullYear()} {footer.copyright} - Crafted with love using{" "}
+            <Link
+              href="https://www.docubook.pro"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline underline-offset-2"
+            >
+              DocuBook
+            </Link>
           </p>
         </div>
 
+        {/* Footer Buttons */}
         <div className="gap-4 items-center hidden md:flex">
           <FooterButtons />
         </div>
@@ -46,7 +44,8 @@ export function FooterButtons() {
             key={index}
             href={button.url}
             target="_blank"
-            className={buttonVariants({ variant: "outline", size: "sm"})}
+            rel="noopener noreferrer"
+            className={buttonVariants({ variant: "outline", size: "sm" })}
           >
             <Icon className="h-4 w-4 mr-2 text-primary" />
             {button.text}
