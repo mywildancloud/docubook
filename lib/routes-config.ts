e80e468254e9a5e91f2357ba34f37679c0448db3
Wildan Nursahidan <80866662+mywildancloud@users.noreply.github.com>
2024-12-21 09:08:46 +0700

    get data from json

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -1,64 +1,28 @@
-// for page navigation & to sort on leftbar
+import docuConfig from "@/docu.json"; // Import JSON file
 
 export type EachRoute = {
   title: string;
   href: string;
-  noLink?: true; // noLink will create a route segment (section) but cannot be navigated
+  noLink?: boolean; // Sekarang mendukung boolean
   items?: EachRoute[];
 };
-// configure menu
-export const ROUTES: EachRoute[] = [
-  {
-    title: "Getting Started",
-    href: "/getting-started",
-    noLink: true,
-    items: [
-      { title: "Introduction", href: "/introduction" },
-      {
-        title: "Installation",
-        href: "/installation",
-      },
-      { title: "Quick Start Guide", href: "/quick-start-guide" },
-      {
-        title: "Project Structure",
-        href: "/project-structure",
-      },
-      {
-        title: "Components",
-        href: "/components",
-        items: [
-          { title: "Stepper", href: "/stepper" },
-          { title: "Tabs", href: "/tabs" },
-          { title: "Note", href: "/note" },
-          { title: "Code Block", href: "/code-block" },
-          { title: "Image & Link", href: "/image-link" },
-          { title: "Youtube", href: "/youtube" },
-          { title: "Tooltips", href: "/tooltips" },
-          { title: "Card", href: "/card" },
-          { title: "Button", href: "/button" },
-          { title: "Custom", href: "/custom" },
-        ],
-      },
-      { title: "Themes", href: "/themes" },
-      {
-        title: "Customize", href: "/customize",
-      },
-    ],
-  },
-];
+
+export const ROUTES: EachRoute[] = docuConfig.routes;
 
 type Page = { title: string; href: string };
 
-function getRecurrsiveAllLinks(node: EachRoute) {
+function getRecursiveAllLinks(node: EachRoute): Page[] {
   const ans: Page[] = [];
   if (!node.noLink) {
     ans.push({ title: node.title, href: node.href });
   }
   node.items?.forEach((subNode) => {
     const temp = { ...subNode, href: `${node.href}${subNode.href}` };
-    ans.push(...getRecurrsiveAllLinks(temp));
+    ans.push(...getRecursiveAllLinks(temp));
   });
   return ans;
 }
 
-export const page_routes = ROUTES.map((it) => getRecurrsiveAllLinks(it)).flat();
+export const page_routes: Page[] = ROUTES.map((route) =>
+  getRecursiveAllLinks(route)
+).flat();
