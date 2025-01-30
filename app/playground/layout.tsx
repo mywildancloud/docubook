e970665fbdb3ac59c6668248bfe33c3d00ec6d7c
Wildan Nursahidan <edpwildan@gmail.com>
2025-01-31 00:29:10 +0700

    fix metadata: playground page

--- app/playground/layout.tsx
+++ app/playground/layout.tsx
@@ -1,4 +1,11 @@
 import { PropsWithChildren } from "react";
+import { getMetadata } from "@/app/layout";
+
+export const metadata = getMetadata({
+    title: "Playground",
+    description: "Test and experiment with DocuBook markdown components in real-time",
+    image: "img-playground.png",
+  });
 
 export default function PlaygroundLayout({ children }: PropsWithChildren) {
   return (
